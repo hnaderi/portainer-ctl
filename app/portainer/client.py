dc73b21b5f0e05264e24c01283aabf51e82a9a2f
Hossein Naderi <hossein-naderi@hotmail.com>
2022-08-01 13:48:51 +0430

    Added stripping to stack name normalization

--- app/portainer/client.py
+++ app/portainer/client.py
@@ -128,7 +128,8 @@ class Endpoint:
       logger.info('existing secret ' + name)
 
   def deploy(self, stack_name:str, compose:str, env_vars):
-    stack_name = stack_name.lower()
+    stack_name = stack_name.lower().strip()
+
     stacks = self.client.get_stack(stack_name)
 
     if len(stacks) == 1:
