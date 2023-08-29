ff8e64ed11c7a861f20dc47ebe4639c99b47fa10
Hossein Naderi <mail@hnaderi.dev>
2023-08-29 16:27:41 +0330

    Added flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Portainer controller development environment";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        python = (pkgs.python3.withPackages
+          (ps: with ps; [ black isort pylsp-mypy requests ]));
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ python ];
+          packages = [ python ];
+        };
+      });
+}
