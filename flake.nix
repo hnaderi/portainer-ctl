48cf2c1ee6757c0450488d07e601d18dcc944d2c
Hossein Naderi <mail@hnaderi.dev>
2023-08-30 17:04:46 +0330

    Poetry packaging

--- flake.nix
+++ flake.nix
@@ -1,18 +1,26 @@
 {
   description = "Portainer controller development environment";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, flake-utils, nixpkgs }:
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
 
-        python = (pkgs.python3.withPackages
-          (ps: with ps; [ black isort pylsp-mypy requests types-requests ]));
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ python ];
-          packages = [ python ];
+        packages = {
+          pctl = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.pctl;
         };
+
+        devShells.default =
+          pkgs.mkShell { packages = [ poetry2nix.packages.${system}.poetry ]; };
       });
 }
