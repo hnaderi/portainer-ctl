79e2941c1082cc5b5277b3df1fc1b69568aad460
Hossein Naderi <hossein-naderi@hotmail.com>
2026-06-10 13:08:42 +0330

    Simplified shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 let
   pkgs = import <nixpkgs> { };
-  python = pkgs.python3.withPackages
-    (ps: with ps; [ pylsp-mypy requests rich rich-argparse black ]);
-in pkgs.mkShell {
+in
+pkgs.mkShell {
   name = "Portainer controller";
-  buildInputs = [ python pkgs.uv ];
+  buildInputs = with pkgs; [
+    python
+    uv
+    ruff
+  ];
 }
